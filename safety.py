b62e44160aaee9150c9cb3e77c38842017daff91
R1chard0418 <36198383+R1chard0418@users.noreply.github.com>
2018-07-01 13:44:09 +0100

    Add files via upload

--- safety.py
+++ safety.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from util import *
+## Need Python version > 2.7
+from collections import Counter
 import numpy as np
 from darkflow.net.build import TFNet
 import cv2
@@ -16,15 +18,28 @@ else:
 
 import base64
 
+## Added different colours for common detected objects
+##(will find a better solution for all possible labels later)
+def decide_box_colour(str):
+    colour_list=[{"label":"person","colour":(255,0,0)},{"label":"bicycle","colour":(0,255,0)},{"label":"car","colour":(0,0,255)},
+                 {"label":"bus","colour":(242,198,90)},{"label":"truck","colour":(144,75,154)},{"label":"motorbike","colour":(237,155,16)},
+                 {"label":"traffic light", "colour":(255,255,0)}]
+    for colour in colour_list:
+        if (str == colour["label"]):
+            colour = colour["colour"]
+            break
+    return colour
+
 ## To draw the bounding boxes for detected objects
 def write_boundingboxes(results, imgcv):
+    colour_list = []
     for result in results:
         cv2.rectangle(imgcv,
                      (result["topleft"]["x"], result["topleft"]["y"]),
                      (result["bottomright"]["x"],result["bottomright"]["y"]),
-                     (0, 255, 0), 2)
+                     decide_box_colour(result["label"]), 2)
         text_x, text_y = int(result["topleft"]["x"]) - 10, int(result["topleft"]["y"]) - 10
-        cv2.putText(imgcv, result["label"], (text_x, text_y),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(imgcv, result["label"], (text_x, text_y),cv2.FONT_HERSHEY_SIMPLEX, 0.8, decide_box_colour(result["label"]), 2, cv2.LINE_AA)
     cv2.imwrite("prediction.png", imgcv)
 
 ## check safety using darkflow
@@ -34,7 +49,7 @@ def check_safety_dflow(step, https, pano, fov, heading, pitch, key):
     else:
         print('step {0} exists'.format(step))
 
-    options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.4}
+    options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.3}
     tfnet = TFNet(options)
 
     url='{0}&pano={1}&fov={2}&heading={3}&pitch={4}&key={5}'.format(https, pano, fov, heading, pitch, key)
@@ -64,12 +79,12 @@ def check_safety_dflow(step, https, pano, fov, heading, pitch, key):
             img = "fov{0}heading{1}pitch{2}.png".format(fov, h, pitch)
             urlretrieve(url, "./{0}/{1}".format(step, img))
             imgcv = cv2.imread("./{0}/{1}".format(step, img))
-            results = tfnet.return_predict(imgcv)
-            write_boundingboxes(results, imgcv)
+            adv_results = tfnet.return_predict(imgcv)
+            write_boundingboxes(adv_results, imgcv)
             adv_labels = []
-            for result in results:
+            for result in adv_results:
                 adv_labels.append(result["label"])
-            if not (set(origin_labels)==set(adv_labels)):
+            if not (Counter(origin_labels)==Counter(adv_labels)):
                 os.system("rm ./{0}/{1}".format(step, img))
                 os.system("cp prediction.png ./{0}/adv-{1}".format(step, img))
                 if step>9:
@@ -82,4 +97,4 @@ def check_safety_dflow(step, https, pano, fov, heading, pitch, key):
                 os.system("rm ./{0}/{1}".format(step, img))
         if adv_found: return False
 
-    return True 
+    return True
