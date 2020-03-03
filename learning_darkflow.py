550a9869185003ae1777ed6e06778c22b8e21659
David Bradley <David@Davids-MacBook-Pro.local>
2020-03-03 10:22:54 +0000

    added warning and danger sectors to left and right of driving path

--- learning_darkflow.py
+++ learning_darkflow.py
@@ -15,15 +15,31 @@ import json
 from darkflow.net.build import TFNet
 
 #creating the darkflow object detector
-options = {"model": "./cfg/yolo.cfg", "load": "./cfg/bin/yolo.weights", "threshold": 0.3}
+options = {"model": "./cfg/yolo.cfg", "load": "./cfg/bin/yolo.weights", "threshold": 0.3, "gpu": 0.6}
 tfnet = TFNet(options)
 
 routes = os.listdir("./test_route/")
 predicted_path = 'predicted_routes'
-driving_path = np.array([[210,195],[130,400],[330,400],[220,195]], np.int32)
+driving_path = np.array([[180,195],[100,400],[300,400],[190,195]], np.int32)
 warning_path = np.array([[210,195],[180,300],[270,300],[220,195]], np.int32)
 danger_path = np.array([[180,300],[130,400],[330,400],[270,300]], np.int32)
-
+warning_left_sector = np.array([[driving_path[1][0],driving_path[0][1]],
+                              [driving_path[1][0],driving_path[0][1]+50],
+                              [driving_path[1][0]+60,driving_path[0][1]+50],
+                              [driving_path[0][0],driving_path[0][1]]], np.int32)
+danger_left_sector = np.array([[warning_left_sector[1][0],warning_left_sector[1][1]],
+                              [0,300],
+                              [0,400],
+                              [driving_path[1][0],driving_path[1][1]],
+                              [warning_left_sector[2][0],warning_left_sector[2][1]]], np.int32)
+warning_right_sector = np.array([[driving_path[3][0],driving_path[3][1]],
+                              [driving_path[3][0]+30,driving_path[3][1]+50],
+                              [driving_path[2][0],driving_path[0][1]+50],
+                              [driving_path[2][0],driving_path[3][1]]], np.int32)
+danger_right_sector = np.array([[warning_right_sector[1][0],warning_right_sector[1][1]],
+                              [driving_path[2][0],driving_path[2][1]],
+                              [driving_path[2][0]+40,driving_path[2][1]],
+                              [warning_right_sector[1][0]+40,warning_right_sector[1][1]]], np.int32)
 
 def decide_box_colour(label):
     colour = (0,0,255)
@@ -44,7 +60,15 @@ def write_boundingboxes(results, imgcv, new_img):
     imgcv = cv2.imread(new_img)
     ## ROI: region of interest
     vtx = driving_path
+    vtx2 = warning_left_sector
+    vtx3 = danger_left_sector
+    vtx4 = warning_right_sector
+    vtx5 = danger_right_sector
     cv2.polylines(imgcv, [vtx], True, (255,255,255), 2)
+    cv2.polylines(imgcv, [vtx2], True, (51,165,255), 2)
+    cv2.polylines(imgcv, [vtx3], True, (0,0,255), 2)
+    cv2.polylines(imgcv, [vtx4], True, (51,165,255), 2)
+    cv2.polylines(imgcv, [vtx5], True, (0,0,255), 2)
     results = check_if_object_in_path(results)
     for result in results:
         if result['label']=='': continue 
@@ -72,6 +96,10 @@ def check_if_object_in_path(results):
         point4 = Point(result['bottomright']['x'], result['topleft']['y'])
         warning_poly = Polygon(warning_path)
         danger_poly = Polygon(danger_path)
+        left_warning_poly = Polygon(warning_left_sector)
+        left_danger_poly = Polygon(danger_left_sector)
+        right_warning_poly = Polygon(warning_right_sector)
+        right_danger_poly = Polygon(danger_right_sector)
         if warning_poly.contains(point1):
             result['label'] = 'warning {0}'.format(result['label'])
         elif warning_poly.contains(point2):
@@ -80,7 +108,25 @@ def check_if_object_in_path(results):
             result['label'] = 'warning {0}'.format(result['label'])
         elif warning_poly.contains(point4):
             result['label'] = 'warning {0}'.format(result['label'])
-
+        
+        if left_warning_poly.contains(point1):
+            result['label'] = 'warning {0}'.format(result['label'])
+        elif left_warning_poly.contains(point2):
+            result['label'] = 'warning {0}'.format(result['label'])
+        elif left_warning_poly.contains(point3):
+            result['label'] = 'warning {0}'.format(result['label'])
+        elif left_warning_poly.contains(point4):
+            result['label'] = 'warning {0}'.format(result['label'])
+        
+        if right_warning_poly.contains(point1):
+            result['label'] = 'warning {0}'.format(result['label'])
+        elif right_warning_poly.contains(point2):
+            result['label'] = 'warning {0}'.format(result['label'])
+        elif right_warning_poly.contains(point3):
+            result['label'] = 'warning {0}'.format(result['label'])
+        elif right_warning_poly.contains(point4):
+            result['label'] = 'warning {0}'.format(result['label'])
+        
         if danger_poly.contains(point1):
             result['label'] = 'danger {0}'.format(result['label'])
         elif danger_poly.contains(point2):
@@ -88,6 +134,24 @@ def check_if_object_in_path(results):
         elif danger_poly.contains(point3):
             result['label'] = 'danger {0}'.format(result['label'])
         elif danger_poly.contains(point4):
+            result['label'] = 'danger {0}'.format(result['label'])
+
+        if left_danger_poly.contains(point1):
+            result['label'] = 'danger {0}'.format(result['label'])
+        elif left_danger_poly.contains(point2):
+            result['label'] = 'danger {0}'.format(result['label'])
+        elif left_danger_poly.contains(point3):
+            result['label'] = 'danger {0}'.format(result['label'])
+        elif left_danger_poly.contains(point4):
+            result['label'] = 'danger {0}'.format(result['label'])
+
+        if right_danger_poly.contains(point1):
+            result['label'] = 'danger {0}'.format(result['label'])
+        elif right_danger_poly.contains(point2):
+            result['label'] = 'danger {0}'.format(result['label'])
+        elif right_danger_poly.contains(point3):
+            result['label'] = 'danger {0}'.format(result['label'])
+        elif right_danger_poly.contains(point4):
             result['label'] = 'danger {0}'.format(result['label'])
     return results
 
