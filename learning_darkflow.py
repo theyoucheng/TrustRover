4a4440f64a63070fea5ae10e9be1dfec8fd0b41b
David Bradley <David@Davids-MacBook-Pro.local>
2020-03-03 11:39:56 +0000

    refactored sector detection

--- learning_darkflow.py
+++ learning_darkflow.py
@@ -43,12 +43,12 @@ danger_right_sector = np.array([[warning_right_sector[1][0],warning_right_sector
 
 def decide_box_colour(label):
     colour = (0,0,255)
-    colour_list=[{"label":"person","colour":(255,0,0)},{"label":"bicycle","colour":(0,255,0)},{"label":"car","colour":(0,255,0)},
-                {"label":"bus","colour":(242,198,90)},{"label":"truck","colour":(144,75,154)},{"label":"motorbike","colour":(237,155,16)},
-                {"label":"traffic light", "colour":(255,255,0)}]
-    for obj in colour_list:
-        if label==obj["label"]: colour = obj["colour"]
-    if colour =='': colour = (0,0,255)
+    # colour_list=[{"label":"person","colour":(255,0,0)},{"label":"bicycle","colour":(0,255,0)},{"label":"car","colour":(0,255,0)},
+    #             {"label":"bus","colour":(242,198,90)},{"label":"truck","colour":(144,75,154)},{"label":"motorbike","colour":(237,155,16)},
+    #             {"label":"traffic light", "colour":(255,255,0)}]
+    # for obj in colour_list:
+    #     if label==obj["label"]: colour = obj["colour"]
+    # if colour =='': colour = (0,0,255)
     if label.startswith("danger"):
         colour = (0,0,255)
     if label.startswith("warning"):
@@ -64,20 +64,20 @@ def write_boundingboxes(results, imgcv, new_img):
     vtx3 = danger_left_sector
     vtx4 = warning_right_sector
     vtx5 = danger_right_sector
-    cv2.polylines(imgcv, [vtx], True, (255,255,255), 2)
-    cv2.polylines(imgcv, [vtx2], True, (51,165,255), 2)
-    cv2.polylines(imgcv, [vtx3], True, (0,0,255), 2)
-    cv2.polylines(imgcv, [vtx4], True, (51,165,255), 2)
-    cv2.polylines(imgcv, [vtx5], True, (0,0,255), 2)
+    # cv2.polylines(imgcv, [vtx], True, (255,255,255), 2)
+    # cv2.polylines(imgcv, [vtx2], True, (51,165,255), 2)
+    # cv2.polylines(imgcv, [vtx3], True, (0,0,255), 2)
+    # cv2.polylines(imgcv, [vtx4], True, (51,165,255), 2)
+    # cv2.polylines(imgcv, [vtx5], True, (0,0,255), 2)
     results = check_if_object_in_path(results)
     for result in results:
-        if result['label']=='': continue 
+        if result['status']=='': continue 
         cv2.rectangle(imgcv,
                      (result["topleft"]["x"], result["topleft"]["y"]),
                      (result["bottomright"]["x"],result["bottomright"]["y"]),
-                     decide_box_colour(result["label"]), 2)
+                     decide_box_colour(result["status"]), 2)
         text_x, text_y = int(result["topleft"]["x"]) - 10, int(result["topleft"]["y"]) - 10
-        cv2.putText(imgcv, result["label"], (text_x, text_y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, decide_box_colour(result['label']), 2, cv2.LINE_AA)
+        cv2.putText(imgcv, result["label"], (text_x, text_y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, decide_box_colour(result['status']), 2, cv2.LINE_AA)
         cv2.imwrite(new_img, imgcv)
 
 def convertToGif(images, path, route):
@@ -87,72 +87,34 @@ def convertToGif(images, path, route):
             img_list.append(imageio.imread('./{0}/{1}/{2}'.format(path, route, image)))
         imageio.mimsave('./{0}/{1}/journey.gif'.format(path, route),img_list, duration=0.2)
 
+def objectInSector(result, poly, message):
+    result['status'] = ''
+    point1 = Point(result['topleft']['x'], result['topleft']['y'])
+    point2 = Point(result['bottomright']['x'], result['bottomright']['y'])
+    point3 = Point(result['topleft']['x'], result['bottomright']['y'])
+    point4 = Point(result['bottomright']['x'], result['topleft']['y'])
+    if poly.contains(point1):
+        result['status'] = message
+    elif poly.contains(point2):
+        result['status'] = message
+    elif poly.contains(point3):
+        result['status'] = message
+    elif poly.contains(point4):
+        result['status'] = message
+    return result
+
+
+
 def check_if_object_in_path(results):
     for result in results:
-        inside = False
-        point1 = Point(result['topleft']['x'], result['topleft']['y'])
-        point2 = Point(result['bottomright']['x'], result['bottomright']['y'])
-        point3 = Point(result['topleft']['x'], result['bottomright']['y'])
-        point4 = Point(result['bottomright']['x'], result['topleft']['y'])
-        warning_poly = Polygon(warning_path)
-        danger_poly = Polygon(danger_path)
-        left_warning_poly = Polygon(warning_left_sector)
-        left_danger_poly = Polygon(danger_left_sector)
-        right_warning_poly = Polygon(warning_right_sector)
-        right_danger_poly = Polygon(danger_right_sector)
-        if warning_poly.contains(point1):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif warning_poly.contains(point2):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif warning_poly.contains(point3):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif warning_poly.contains(point4):
-            result['label'] = 'warning {0}'.format(result['label'])
+        result['status'] = ''
+        result = objectInSector(result, Polygon(warning_path), "warning" )
+        result = objectInSector(result, Polygon(warning_left_sector), "warning" )
+        result = objectInSector(result, Polygon(warning_right_sector), "warning" )
+        result = objectInSector(result, Polygon(danger_path), "danger" )
+        result = objectInSector(result, Polygon(danger_left_sector), "danger" )
+        result = objectInSector(result, Polygon(danger_right_sector), "danger" )
         
-        if left_warning_poly.contains(point1):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif left_warning_poly.contains(point2):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif left_warning_poly.contains(point3):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif left_warning_poly.contains(point4):
-            result['label'] = 'warning {0}'.format(result['label'])
-        
-        if right_warning_poly.contains(point1):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif right_warning_poly.contains(point2):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif right_warning_poly.contains(point3):
-            result['label'] = 'warning {0}'.format(result['label'])
-        elif right_warning_poly.contains(point4):
-            result['label'] = 'warning {0}'.format(result['label'])
-        
-        if danger_poly.contains(point1):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif danger_poly.contains(point2):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif danger_poly.contains(point3):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif danger_poly.contains(point4):
-            result['label'] = 'danger {0}'.format(result['label'])
-
-        if left_danger_poly.contains(point1):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif left_danger_poly.contains(point2):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif left_danger_poly.contains(point3):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif left_danger_poly.contains(point4):
-            result['label'] = 'danger {0}'.format(result['label'])
-
-        if right_danger_poly.contains(point1):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif right_danger_poly.contains(point2):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif right_danger_poly.contains(point3):
-            result['label'] = 'danger {0}'.format(result['label'])
-        elif right_danger_poly.contains(point4):
-            result['label'] = 'danger {0}'.format(result['label'])
     return results
 
 
